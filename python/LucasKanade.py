ffd86650360034d527b8a1da1c405a44259a348c
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2023-03-04 10:25:04 -0500

    switch to flatten()

--- python/LucasKanade.py
+++ python/LucasKanade.py
@@ -33,19 +33,16 @@ def LucasKanade(It, It1, rect):
     It_spline = RectBivariateSpline(y, x, It)       # interpolated = spline(indices[0], indices[1], grid=False)
     warped_spline = RectBivariateSpline(y, x, img_warped)
 
-    # gradient
-    ### use RectBivariateSpline.ev
-    # x1, y1 = spline.ev()
-
     # unroll matrices into the right vectors
-    I = np.ravel(warped_spline(np.arange(x1, x2+1), np.arange(y1, y2+1)))      # need to subtract with below, so should be same shape
-    T = np.ravel(It_spline(np.arange(x1, x2+1), np.arange(y1, y2+1)))              # should just be rectangular portion of It image
+    I = np.ndarray.flatten(warped_spline(np.arange(x1, x2+1), np.arange(y1, y2+1)))      # need to subtract with below, so should be same shape
+    T = np.ndarray.flatten(It_spline(np.arange(x1, x2+1), np.arange(y1, y2+1)))              # should just be rectangular portion of It image
 
     # image gradient
     xs, ys = np.meshgrid(x, y)
     I_x = warped_spline.ev(xs, ys, dx=1)    # image gradient, not spline
     I_y = warped_spline.ev(xs, ys, dy=1)
 
+    # said do not need to get spline again, but I_x is an ndarray, not a spline, how to access points in?
     I_x_spline = RectBivariateSpline(y, x, I_x)
     I_y_spline = RectBivariateSpline(y, x, I_y)
 
@@ -53,8 +50,8 @@ def LucasKanade(It, It1, rect):
     I_y = I_y_spline(np.arange(x1, x2+1), np.arange(y1, y2+1))
 
     # unroll gradients
-    I_x = np.ravel(I_x)
-    I_y = np.ravel(I_y)
+    I_x = np.ndarray.flatten(I_x)
+    I_y = np.ndarray.flatten(I_y)
     I_grad = np.stack((I_x, I_y), 1)
 
     # error image
